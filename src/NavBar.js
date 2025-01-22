1d0687b2d288d6b419d395bd663e5d3e6ae08f3f
HasTeriZ <yangweijei@gmail.com>
2025-01-22 17:43:03 +0100

    NavBar + cancellazione di cose inutili

---
+++ src/NavBar.js
@@ -0,0 +1,33 @@
+function NavBar() {
+    return <>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <img src="C:\Users\pc\Desktop\ToStudy\public\favicon.ico" className="rounded mx-auto d-block" alt="..." />
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">I miei appunti</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Esplora</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" aria-disabled="true" href="#">Gruppi</a>
+          <li className="nav-item">
+          <a className="nav-link" aria-disabled="true" href="#">Faq</a>
+        </li>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    </>
+}
+
+export default NavBar;
