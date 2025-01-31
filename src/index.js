aa3739f244f34f97017d84b8e18a418c599e78dd
HasTeriZ <yangweijei@gmail.com>
2025-01-31 16:29:00 +0100

    Fix

--- src/index.js
+++ src/index.js
@@ -1,38 +1,28 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './NavBar';
-import Register from './Register';
-import './index.css';
-import AppuntiPage from './AppuntiPage';
-import EsploraPage from './EsploraPage';
-import GruppiPage from './GruppiPage';
-import FaqPage from './FaqPage';
-import HomePage from './HomePage';
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./NavBar";
 import Register from "./Register";
 import "./index.css";
-import Login from "./Login";
+import AppuntiPage from "./AppuntiPage";
+import EsploraPage from "./EsploraPage";
+import GruppiPage from "./GruppiPage";
+import FaqPage from "./FaqPage";
+import HomePage from "./HomePage";
+import "./index.css";
 import ImageCarousel from "./ImageCarousel";
->>>>>>> 2ba000f55241321a755931344c700a7c686c76c3
+import Login from "./Login";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <NavBar />
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<HomePage />} />
         <Route path="/appunti" element={<AppuntiPage />} />
         <Route path="/esplora" element={<EsploraPage />} />
         <Route path="/gruppi" element={<GruppiPage />} />
         <Route path="/faq" element={<FaqPage />} />
-=======
         <Route path="/" element={<div>Home Page</div>} />
         <Route
           path="/login"
@@ -43,14 +33,9 @@ ReactDOM.render(
             </>
           }
         />
->>>>>>> 2ba000f55241321a755931344c700a7c686c76c3
         <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   </React.StrictMode>,
-<<<<<<< HEAD
-  document.getElementById('root')
-=======
   document.getElementById("root")
->>>>>>> 2ba000f55241321a755931344c700a7c686c76c3
 );
