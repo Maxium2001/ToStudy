df50a92b41936ea803c219eba60de6e0a3fbb612
Maxium2001 <162455993+Maxium2001@users.noreply.github.com>
2025-02-02 19:14:34 +0100

    inizio home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,40 +4,25 @@ import './Function.css';
 
 function HomePage() {
     return (
-        <div className="homepage-container">
-            {/* Prima sezione */}
-            <div className="section">
-                <div className="section-row">
-                    <h2>Zona 1</h2>
-                    <p>Contenuti per la seconda zona.</p>
-                </div>
-                <div className="divider-row"></div>
-                <div className="section-row">
-                    <h2>Zona 2</h2>
-                    <p>Contenuti per la seconda zona.</p> 
-                </div>
-            </div>
-           
-            {/* Linea divisoria */}
-            <div className="divider"></div>
-
-            {/* Seconda sezione */}
-            <div className="section">
-                <h2>Zona 3</h2>
-                <p>Contenuti per la seconda zona.</p>
-            </div>
-
-            {/* Linea divisoria */}
-            <div className="divider"></div>
-
-            {/* Terza sezione */}
-            <div className="section">
-                <h2>Zona 4</h2>
-                <p>Contenuti per la terza zona.</p>
-            </div>
+        <div class="homepage">
+        <div class="column">
+            <div class="neutral-zone"></div>
+            <hr class="separator" />
+            <div class="box clickable"></div>
+            <div class="box clickable"></div>
+            <div class="box clickable"></div>
         </div>
+        <div class="column">
+            <div class="box clickable"></div>
+            <div class="box clickable"></div>
+            <div class="box clickable"></div>
+        </div>
+        <div class="column">
+            <div class="box clickable"></div>
+            <div class="box clickable"></div>
+            <div class="box clickable"></div>
+        </div>
+    </div>
     );
 }
-
-
 export default HomePage;
