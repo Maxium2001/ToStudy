55d57b9dfdb875e589d67a31fe6ab6bee2a8b52a
Maxium2001 <162455993+Maxium2001@users.noreply.github.com>
2025-02-04 17:43:21 +0100

    cambiata homepage,visualizzabile sia mobile che non

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,55 +1,72 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import './Style.css';
 
 function HomePage() {
   const [clickBoxes, setClickBoxes] = useState([]);
+  const containerRef = useRef(null);
 
   useEffect(() => {
-    // Carica le click box dalla memoria locale
     const storedBoxes = JSON.parse(localStorage.getItem('clickBoxes')) || [];
     setClickBoxes(storedBoxes);
   }, []);
 
-    return (
-        <div className="homepage">
-            <div className="column">
-            <h1>PROFILO</h1> 
-                <div className="neutral-zone"></div>
-                <a href="ProfiloPage.js" className="circle-icon clickable"></a>
-                <hr className="separator" />
-                <h1>APPUNTI</h1> 
-                {/* Colonna Scrollabile: Questo div è scrollabile se il contenuto è più grande */}
-                <div class="click-box-container" id="clickBoxesContainer1">
-                   {clickBoxes.map((box, index) => (
-               <div className="box clickable" key={index}>
-                   {box.title}
-              </div>
-                 ))}
-             </div>
-          </div>
-            <div className="column">
-            <h1>GRUPPI</h1> 
-            <div class="click-box-container" id="clickBoxesContainer1">
-                   {clickBoxes.map((box, index) => (
-               <div className="box clickable" key={index}>
-                   {box.title}
-              </div>
-                    ))}
-                </div>
-            </div>
-            <div className="column">
-            <h1>SCHEDA DI OGGI</h1> 
-            <div class="click-box-container" id="clickBoxesContainer1">
-                   {clickBoxes.map((box, index) => (
-               <div className="box clickable" key={index}>
-                   {box.title}
-              </div>
-                 ))}
-            </div>
+  // Funzione per gestire lo swipe
+  useEffect(() => {
+    const container = containerRef.current;
+    let startX = 0;
+
+    const handleTouchStart = (e) => {
+      startX = e.touches[0].clientX;
+    };
+
+    const handleTouchMove = (e) => {
+      const diffX = startX - e.touches[0].clientX;
+      container.scrollLeft += diffX;
+      startX = e.touches[0].clientX;
+    };
+
+    container.addEventListener('touchstart', handleTouchStart);
+    container.addEventListener('touchmove', handleTouchMove);
+
+    return () => {
+      container.removeEventListener('touchstart', handleTouchStart);
+      container.removeEventListener('touchmove', handleTouchMove);
+    };
+  }, []);
+
+  return (
+    <div className="homepage">
+      {/* Zona neutra - visibile sopra le colonne su mobile */}
+  
+      <div className="scroll-container" ref={containerRef}>
+        <div className="column">
+          <h1>PROFILO</h1> 
+
+          <h1>APPUNTI</h1> 
+          <ClickBoxContainer clickBoxes={clickBoxes} />
         </div>
+        <div className="column">
+          <h1>GRUPPI</h1> 
+          <ClickBoxContainer clickBoxes={clickBoxes} />
+        </div>
+      </div>
     </div>
-    );
+  );
+}
+
+function ClickBoxContainer({ clickBoxes }) {
+  return (
+    <div className="click-box-container">
+      {clickBoxes.map((box, index) => (
+        <div className="box clickable" key={index}>
+          {box.title}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default HomePage;
+
 
